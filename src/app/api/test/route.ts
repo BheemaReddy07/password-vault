369d638a4fa4ae3dc020e6d42bf6f2a05ac19cc1
BheemaReddy07 <bheemareddy2910@gmail.com>
2025-10-05 12:13:06 +0530

    DB setup

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+import dbConnect from "@/lib/mongodb";
+export async function GET(){
+    try {
+        await dbConnect();
+        return NextResponse.json({message:"MongoDB connected successfully ✅"},{status:200});
+    } catch (error) {
+        return NextResponse.json({message:"MongoDB connection failed ❌"},{status:500} );
+    }
+}
